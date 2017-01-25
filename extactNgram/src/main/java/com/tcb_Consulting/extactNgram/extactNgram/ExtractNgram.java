c4333a02803b51a1f5b235fec589f40ab95d2494
abdassatar <bouazizi.abdessatar@gmail.com>
2017-01-25 09:48:28 +0100

    Update ExtractNgram.java

--- extactNgram/src/main/java/com/tcb_Consulting/extactNgram/extactNgram/ExtractNgram.java
+++ extactNgram/src/main/java/com/tcb_Consulting/extactNgram/extactNgram/ExtractNgram.java
@@ -1,6 +1,8 @@
 package com.tcb_Consulting.extactNgram.extactNgram;
+import java.io.IOException;
 
 public interface ExtractNgram {
-	public abstract void extractNgram();
+	
+	 public void extractNgram(String text, int length, Boolean stopWords, Boolean overlap) throws IOException;
 
 }
